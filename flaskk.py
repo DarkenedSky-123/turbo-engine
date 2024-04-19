084cdf096484ae17ffb1af4ca18999fbef381be3
Armx <114233771+DarkenedSky-123@users.noreply.github.com>
2024-04-19 22:39:02 +0530

    Update flaskk.py

--- flaskk.py
+++ flaskk.py
@@ -1,7 +1,7 @@
 import os
 FILE = './file.txt'
 gmail = "armx94522@gmail.com"
-name="DarkenedSky-123"
+name = "DarkenedSky-123"
 os.system(f"git config --global user.email {gmail}")
 os.system(f"git config --global user.name {name}")
 os.system(f'git add {FILE}')
