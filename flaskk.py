be1ecb06c291b54aa202e93eed4658d02b5c2a43
Armx <114233771+DarkenedSky-123@users.noreply.github.com>
2024-04-19 21:57:34 +0530

    Update flaskk.py

--- flaskk.py
+++ flaskk.py
@@ -1,14 +1,4 @@
-import git
-
-# Assuming your git repository is located in the same directory as your Python code
-repo = git.Repo('.')
-
 # Open your data.txt file in append mode
 with open("./data.txt", "a") as f:
     f.write("Hello\n")
 
-# Stage the changes made to the file
-repo.index.add(["data.txt"])
-
-# Commit the changes with a custom commit message
-repo.index.commit("Added content to data.txt")
