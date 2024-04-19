9011addb163d43662adff3b2367c158658d96a26
Armx <114233771+DarkenedSky-123@users.noreply.github.com>
2024-04-19 22:38:00 +0530

    Update flaskk.py

--- flaskk.py
+++ flaskk.py
@@ -1,4 +1,8 @@
 import os
 FILE = './file.txt'
+gmail = "armx94522@gmail.com"
+name="DarkenedSky-123"
+os.system(f"git config --global user.email {gmail}")
+os.system(f"git config --global user.name {name}")
 os.system(f'git add {FILE}')
 os.system("git commit -m 'update file'")
